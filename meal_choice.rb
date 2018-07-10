ce576b33f7f28ce300bc91f1ca203811c889614d
shannonbee <shannonbenyamin@gmail.com>
2018-07-10 18:31:48 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,20 +1,23 @@
 def snacks(food="Cheetos")
   "Any time is the best time for #{food}!"
-
+end
+snacks
 def breakfast(food="frosted flakes")
    "Morning is the best time for #{food}!"
 end
-  
+ breakfast 
   
   def lunch(food ="grilled cheese")
     "Afternoon is the best time for#{food}!"
   end
+  lunch
   
   def dinner(food="salmon")
     "Evening is the best time for #{food}!"
   end
   
-  
+  dinner 
+
   puts breakfast("pancakes")
   puts lunch("chicken")
   puts dinner("soup")
