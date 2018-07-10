698daf99aea95e74a50fa61a793f867e50f99d5e
shannonbee <shannonbenyamin@gmail.com>
2018-07-10 18:38:17 +0000

    Done.

---
+++ food.rb
@@ -0,0 +1,25 @@
+def snack(food="Cheetos")
+  puts "Any time is the best time for "
+
+def breakfast(food="frosted flakes")
+  puts "Morning is the best time for #{food}!"
+end
+  
+  
+  def lunch(food ="grilled cheese")
+    puts "Afternoon is the best time for #{food}!"
+  end
+  
+  def dinner(food="salmon")
+    puts "Evening is the best time for #{food}!"
+  end
+  
+  
+  puts breakfast("pancakes")
+  puts lunch("chicken")
+  puts dinner("soup")
+  
+  puts breakfast
+  puts lunch
+  puts dinner
+  
